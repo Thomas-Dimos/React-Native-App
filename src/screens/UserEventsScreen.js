7a03a71930c4f375d98a26b2b18bd2e615511ec9
Thomas-Dimos <tomdimos55@gmail.com>
2019-04-17 16:27:07 +0300

    Added UserEventsScreen

---
+++ src/screens/UserEventsScreen.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text,TouchableHighlight,ScrollView } from 'react-native';
+import HttpRequest from '../HTTPRequest';
+import localDatabase from '../SQLiteDatabase';
+
+export default class UserEventsScreen extends Component {
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            null
+        )
+    }
+}
